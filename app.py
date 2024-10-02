8b7add755be04d5840a1e61d16fe083f64ef00ea
ninomal <137447782+ninomal@users.noreply.github.com>
2024-10-01 21:44:37 -0300

    Update(products): add getLen and readXLSXiloc

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from products.products import Products
 def main():
     productsService = ProductsService()
     products = Products()
-
-    print(products.readXlsx(r"D:\Users\User\Desktop\CURSO PYTON\DiarioPython\2024.xlsx"))
+    rowLen = products.getLenXlsx(r"D:\Users\User\Desktop\CURSO PYTON\DiarioPython\2024.xlsx")
+    #print(products.readXlsxRowSelect(r"D:\Users\User\Desktop\CURSO PYTON\DiarioPython\2024.xlsx", "DIA"))
+    
+    for i in range(rowLen):
+        print(products.readXlsxIloc(r"D:\Users\User\Desktop\CURSO PYTON\DiarioPython\2024.xlsx", i))
 
     #productsService.addDataOracle(data)
     #print(productsService.exemple())
--- products/products.py
+++ products/products.py
@@ -4,23 +4,52 @@ class Products():
     def __init__(self):
         pass
     
-    def readXlsx(self, path):
+    def readXlsxRowSelect(self, path, columName):
         data = []
         try:
             df= pd.read_excel(path, engine='openpyxl')
-            rowData = df[df['DIA']== 12]
-            #data.append(rowData.to_dict(orient='records'))
-            #return data[0]
-
+            rowData = df[df[f'{columName}']== 12]
+        
             #dict FILA
             data.append(rowData.to_dict(orient='records'))
             return data[0]
+        
         except FileNotFoundError:
             return ("ERROR PATH")
         except ValueError:
             return ("Valuer ERROR")
         except IndexError:
             return ("INDEX ERROR")
+        
+    def readXlsxIloc(self, path, row):
+        data = []
+        try:
+            df= pd.read_excel(path, engine='openpyxl')
+            rowData = df.iloc[row]
+        
+            #dict FILA
+            data.append(rowData.to_dict())
+            return data[0]
+        
+        except FileNotFoundError:
+            return ("ERROR PATH")
+        except ValueError:
+            return ("Valuer ERROR")
+        except IndexError:
+            return ("INDEX ERROR")
+ 
+    def getLenXlsx(self, path):
+        try:
+            df= pd.read_excel(path, engine='openpyxl')
+            return (len(df)-1)
+        
+        except FileNotFoundError:
+            return ("ERROR PATH")
+        except ValueError:
+            return ("Valuer ERROR")
+        except IndexError:
+            return ("INDEX ERROR")
+        
       
 
         
